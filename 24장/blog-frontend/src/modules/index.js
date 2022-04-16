3a38bfb83cddd6e454f398a6aef7fb7186171c77
chaeeunkim <rlacodms3447@naver.com>
2022-04-17 03:45:44 +0900

    [ADD]25장 글쓰기 기능

--- 24장/blog-frontend/src/modules/index.js
+++ 24장/blog-frontend/src/modules/index.js
@@ -4,16 +4,18 @@ import { all } from 'redux-saga/effects';
 import auth, { authSaga } from './auth';
 import loading from './loading';
 import user, { userSaga } from './user';
+import write, { writeSaga } from './write';
 
 const rootReducer = combineReducers({
     auth,
     loading,
     user,
+    write,
 });
 
 // 프로젝트의 rootSaga
 export function* rootSaga() {
-    yield all([authSaga(), userSaga()]);
+    yield all([authSaga(), userSaga(), writeSaga()]);
 }
 
 export default rootReducer;
--- 24장/blog-frontend/src/pages/WritePage.js
+++ 24장/blog-frontend/src/pages/WritePage.js
@@ -1,6 +1,18 @@
 /* 글쓰기 라우트 컴포넌트 */
+import React from 'react';
+import Responsive from '../components/common/Responsive';
+import EditorContainer from '../containers/write/EditorContainer';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
+import WriteActionButtonsContainer from '../containers/write/WriteActionButtonsContainer';
+
 const WritePage = () => {
-    return <div>글쓰기</div>;
+  return (
+    <Responsive>
+      <EditorContainer />
+      <TagBoxContainer />
+      <WriteActionButtonsContainer />
+    </Responsive>
+  );
 };
 
 export default WritePage;
